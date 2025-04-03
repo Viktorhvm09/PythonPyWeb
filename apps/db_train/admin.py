def08a0a1767fe724804534dec6a1a28521df65a
vikthar <hvm09@rambler.ru>
2025-04-03 21:22:15 +0300

    Lab 2 rev 1

--- apps/db_train/admin.py
+++ apps/db_train/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 # Зарегистрируйте свои модели в админ панели здесь
-from .models import Author
+from .models import Author, AuthorProfile, Entry, Tag
 
 admin.site.register(Author)
+admin.site.register(AuthorProfile)
+admin.site.register(Entry)
+admin.site.register(Tag)
